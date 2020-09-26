ade4f5044cc0563a5e1f710c77aedf2526490b55
ScarabGithub <30386360+ScarabGithub@users.noreply.github.com>
2020-09-26 21:41:25 +0530

    Update secondlargestele.c
    When no second largest was found, it would produce the largest number as output.
    
    Program was unable to output the expected output for the following case:
    
    SAMPLE INPUT
    3 3 3 3 3 -1 
    
    EXPECTED OUTPUT 
    0
    
    ACTUAL OUTPUT 
    3
    
    I fixed this by adding a check variable in an  "if max == second" block at the very end of  the only while loop in the program.

--- Assignment-1/secondlargestele.c
+++ Assignment-1/secondlargestele.c
@@ -2,47 +2,52 @@
 
 int main()
 {
-	int curr;            /* current input number */
-	int max = -32768;    /* maximum element seen so far, 
-                              * initialized to smallest int value 
+    int curr;            /* current input number */
+    int max = -32767;    /* maximum element seen so far,
+                              * initialized to smallest int value
                               */
-	int second = -32768; /* second largest element seen so far,
+    int second = -32768;/* second largest element seen so far,
                               * initialized to the smallest int value
                               */
-	scanf("%d",&curr);
-	while(curr != -1){
-                /* 3 cases which can occur
-                 *    1. second <= max < current
-                 *    2. second < current <= max
-                 *    3. current <= second < max
-                 */
-		if(curr > max)
-		{ /* Case 1. */
-                        /* in this case, 
-                         * second is reset to max, and max is reset to current.
-                         * Reason: current is the largest input seen so far,
-                         *         and the earlier max, the second largest.
-                         */
-			second = max;			
-			max = curr;
-		}
-		else
-		{
-			if(curr > second)
-			{ /* Case 2 */
-                        	 /* Reset second to current. Leave max unchanged.
-                                  * Reason: current is the second largest input seen so far.
-                                  *         max is still the largest seen so far.
-                                  */
-				second = curr;
-			}
-		}
-                /* 
-                 * In case 3, we don't need to change max or second
-                 */
-		scanf("%d",&curr);
-	}
+    int check = 0;
 
-	printf("%d\n",second);
-	return 0;
+    while(curr != -1){
+        scanf("%d",&curr);
+        /* 3 cases which can occur
+         *    1. second <= max < current
+         *    2. second < current <= max
+         *    3. current <= second < max
+         */
+        if(curr > max)
+        { /* Case 1. */
+            /* in this case,
+             * second is reset to max, and max is reset to current.
+             * Reason: current is the largest input seen so far,
+             *         and the earlier max, the second largest.
+             */
+            second = max;
+            max = curr;
+            check = 1;
+        }
+        else if(curr > second)
+            { /* Case 2 */
+                /* Reset second to current. Leave max unchanged.
+                     * Reason: current is the second largest input seen so far.
+                     *         max is still the largest seen so far.
+                     */
+                second = curr;
+                check = 1;
+            }
+         if(second == max) {
+            check = 0;
+        }
+
+
+    }
+    if(check == 1)
+    printf("%d",second);
+    else
+    printf("No second largest element found but the largest element is %d",max);
+
+    return 0;
 }
